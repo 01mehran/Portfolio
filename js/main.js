2344a04106c3d73fb41e1961e44f1b33a87a5b67
mehran <mehranmohamadi1311@gmail.com>
2026-01-14 14:58:43 +0330

    refactor: move auto typing logic to a separate file

---
+++ js/main.js
@@ -0,0 +1,86 @@
+window.addEventListener("load", () => {
+  const preLoader = document.querySelector("#preLoading");
+  preLoader.style.display = "none";
+});
+
+// Selecting Elements;
+const _ = document;
+const openMenu = _.querySelector("#menubar");
+const closeBtn = _.querySelector("#close-btn");
+const mobileMenu = _.querySelector("#mobile-menu");
+const ul = _.querySelector("header ul");
+const indicatoreBtn = _.querySelector("#indicatore");
+const header = _.querySelector("header");
+const form = document.querySelector("#form");
+const links = mobileMenu.querySelectorAll("a");
+
+const toggleMobileMenu = function (width, display) {
+  mobileMenu.style.right = width;
+  indicatoreBtn.style.display = display;
+  overlay.classList.toggle("overlay");
+};
+
+mobileMenu.addEventListener("click", (e) => {
+  if (e.target.tagName === "A") {
+    toggleMobileMenu("-400px", "none");
+  }
+});
+
+// smooth Scrolling;
+ul.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.tagName === "A") {
+    const id = e.target.getAttribute("href");
+    _.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+});
+
+// Back to top;
+indicatoreBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+// Height of page;
+window.onscroll = () => {
+  let height = (scrollY / (document.body.scrollHeight - innerHeight)) * 100;
+  indicatoreBtn.style.background = `conic-gradient(white ${height}%, transparent ${height}% 100%)`;
+
+  if (window.scrollY > 100) {
+    indicatoreBtn.style.transform = "translateX(-150%)";
+    header.style.backgroundColor = "black";
+  } else {
+    indicatoreBtn.style.transform = "translateX(0%)";
+    indicatoreBtn.style.opacity = "0.7";
+    header.style.backgroundColor = "transparent";
+  }
+};
+
+// Create overlay;
+const overlay = document.createElement("div");
+header.appendChild(overlay);
+
+overlay.addEventListener("click", () => {
+  toggleMobileMenu("-400px", "bloxk");
+});
+
+// Open Button;
+openMenu.addEventListener("click", () => {
+  toggleMobileMenu("0px", "none");
+});
+
+// Close Button
+closeBtn.addEventListener("click", () => {
+  toggleMobileMenu("-400px", "block");
+});
+
+// Reset form after submiting;
+form.addEventListener("submit", function () {
+  form.reset();
+});
