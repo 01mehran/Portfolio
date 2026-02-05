e3422a22057c0c561d18ac519f3378bec7b5f891
mehran <mehranmohamadi1311@gmail.com>
2026-02-05 15:25:02 +0330

    fix: fix images path

--- js/projects.js
+++ js/projects.js
@@ -1,50 +1,50 @@
-const images = "../images/project_demo_img/";
+const images = "../images/";
 
 export const projectsDetails = [
   {
-    src: `${images}clock.png`,
+    src: `${images}project_demo_img/clock.png`,
     projectName: "Analog Clock",
     projectIcon: "bxs-watch",
     demoUrl: "https://dark-analog-clock.vercel.app/",
   },
 
   {
-    src: `${images}passGen.png`,
+    src: `${images}project_demo_img/passGen.png`,
     projectName: "PassGeneratore",
     projectIcon: "bxs-lock-alt",
     demoUrl: "https://password-generator-two-omega-77.vercel.app/",
   },
 
   {
-    src: `${images}calculatore.png`,
+    src: `${images}project_demo_img/calculatore.png`,
     projectName: "Calculator",
     projectIcon: "bx-calculator",
     demoUrl: "https://calculator-delta-six-27.vercel.app/",
   },
 
   {
-    src: `${images}UpdateCssVar.png`,
+    src: `${images}project_demo_img/UpdateCssVar.png`,
     projectName: "Edite image",
     projectIcon: "bx-edit",
     demoUrl: "https://update-css-variables-with-js-nu.vercel.app/",
   },
 
   {
-    src: `${images}To-DoListApp.png`,
+    src: `${images}project_demo_img/To-DoListApp.png`,
     projectName: "To-Do list app",
     projectIcon: "bx-checkbox-checked",
     demoUrl: "https://to-do-list-app-seven-zeta.vercel.app/",
   },
 
   {
-    src: `${images}Stopwatch.png`,
+    src: `${images}project_demo_img/Stopwatch.png`,
     projectName: "StopWatch",
     projectIcon: "bx-stopwatch",
     demoUrl: "https://stop-watch-sigma-six.vercel.app/",
   },
 
   {
-    src: `${images}cryptoExchange.png`,
+    src: `${images}project_demo_img/cryptoExchange.png`,
     projectName: "Crypto exchange",
     projectIcon: "",
     demoUrl: "https://01mehran.github.io/Crypto-Exchange-App/",
@@ -53,31 +53,31 @@ export const projectsDetails = [
 
 export const mySkills = [
   {
-    skillIcon: "../images/Skills-icon/html-icon.webp",
+    skillIcon: `${images}Skills-icon/html-icon.webp`,
     name: "HTMl",
   },
   {
-    skillIcon: "../images/Skills-icon/css-icon.webp",
+    skillIcon: `${images}Skills-icon/css-icon.webp`,
     skillName: "CSS",
   },
   {
-    skillIcon: "../images/Skills-icon/Js-icon.webp",
+    skillIcon: `${images}Skills-icon/Js-icon.webp`,
     skillName: "javascript",
   },
   {
-    skillIcon: "../images/Skills-icon/VsCode-icon.webp",
+    skillIcon: `${images}Skills-icon/VsCode-icon.webp`,
     skillName: "vs code",
   },
   {
-    skillIcon: "../images/Skills-icon/tailwind-css-icon.webp",
+    skillIcon: `${images}Skills-icon/tailwind-css-icon.webp`,
     skillName: "tailwind",
   },
   {
-    skillIcon: "../images/Skills-icon/figma-icon.webp",
+    skillIcon: `${images}Skills-icon/figma-icon.webp`,
     skillName: "figma",
   },
   {
-    skillIcon: "../images/Skills-icon/git.png",
+    skillIcon: `${images}Skills-icon/git.png`,
     skillName: "git",
   },
 ];
